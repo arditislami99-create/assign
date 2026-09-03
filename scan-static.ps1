14b47438f7f18ccc948a39d2d0f91ad3a7a7800e
Ardit Islami <ardit.islami99@gmail.com>
2026-09-03 20:00:18 +0200

    chore: remove temporary bundle scan scripts

--- scan-static.ps1
+++
@@ -1,8 +0,0 @@
-$files = Get-ChildItem -Recurse -Filter "*.js" .next/static/chunks | Where-Object { $_.Length -gt 1024 }
-foreach ($f in $files) {
-  $b = Get-Content $f.FullName -Raw
-  $c1 = [regex]::Matches($b, 'static\{').Count
-  $c2 = [regex]::Matches($b, '#[A-Za-z_$][\w$]*\s+in\s').Count
-  if ($c1 -gt 0 -or $c2 -gt 0) { Write-Output "$($f.Name): static{=$c1 private-in=$c2" }
-}
-Write-Output "done"
